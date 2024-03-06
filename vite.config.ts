c65644956da0fc37e1030c4bb637f6a63bf0cfe2
Anthony Hook <tone4hook@users.noreply.github.com>
2024-03-06 14:07:22 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'poly-form-canvas',
+  base: '/poly-form-canvas/',
   plugins: [vue()],
 });
